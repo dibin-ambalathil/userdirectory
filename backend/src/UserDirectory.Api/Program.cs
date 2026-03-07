fe09852e786b54d1cb2edc525cf0b60637bd8175
idc.dibin@gmail.com <idc.dibin@gmail.com>
2026-03-07 22:28:27 +0530

    cors

--- backend/src/UserDirectory.Api/Program.cs
+++ backend/src/UserDirectory.Api/Program.cs
@@ -22,7 +22,8 @@ var allowedOrigins = (configuredOrigins is { Length: > 0 }
         "http://127.0.0.1:5173",
         "https://127.0.0.1:5173",
         "http://localhost:3000",
-        "http://127.0.0.1:3000"
+        "http://127.0.0.1:3000",
+        "https://udtest.netlify.app"
     })
     .Distinct(StringComparer.OrdinalIgnoreCase)
     .ToArray();
